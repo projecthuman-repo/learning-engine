28b25d9dd47578cb68cdebdd35fd39f69995b809
jl1600 <jack.liu1600@gmail.com>
2023-02-19 15:53:52 -0500

    pdf extractor path refactor

---
+++ server/models/PDFExtract/pdf_extractor_constants.py
@@ -0,0 +1,4 @@
+TESSERACT_EXE_PATH = r'D:\Program Files\Tesseract-OCR\tesseract.exe'
+TESSERACT_DATA_PATH = '--tessdata-dir "D:\Program Files\Tesseract-OCR\\tessdata"'
+POPPLER_PATH = r"D:\poppler-0.68.0_x86\poppler-0.68.0\bin"
+TEMP_PDF_PATH = "D:\pycharm_projecs\learning-engine\Academic_papers\\1-s2.0-S0092867421000118-main.pdf"
