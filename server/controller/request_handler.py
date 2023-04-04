54c827e949c5fc2358b4e3c16ac43039fb57bb62
Heidi Toews <81480268+heiditoews@users.noreply.github.com>
2023-04-03 19:50:07 -0600

    Update request_handler.py

--- server/controller/request_handler.py
+++ server/controller/request_handler.py
@@ -1,3 +1,14 @@
+import sys
+import os
+myDir = os.getcwd()
+sys.path.append(myDir)
+
+from pathlib import Path
+path = Path(myDir)
+a=str(path.parent.absolute())
+
+sys.path.append(a)
+
 import threading
 
 import uvicorn
